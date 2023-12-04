097396a2a88b81f52f71d39d1c23a103f7e3f531
zenrsr <ragasandeepreddy25@gmail.com>
2023-12-04 10:02:17 +0000

    second commit

--- src/components/CowinDashboard/index.js
+++ src/components/CowinDashboard/index.js
@@ -66,42 +66,6 @@ class CowinDashboard extends Component {
   //     )
   //   }
 
-  //   renderHalfPieChart = () => {
-  //     const {vaccineData} = this.state
-  //     return (
-  //       <div className="mini-container">
-  //         <div className="chart-container">
-  //           <h1 className="chart-title">Vaccination by Gender</h1>
-  //           <ResponsiveContainer width="100%" height={300}>
-  //             <PieChart>
-  //               <Pie
-  //                 cx="50%"
-  //                 cy="60%"
-  //                 data={vaccineData.vaccination_by_gender}
-  //                 startAngle={180}
-  //                 endAngle={0}
-  //                 innerRadius="40%"
-  //                 outerRadius="80%"
-  //                 dataKey="count"
-  //               >
-  //                 <Cell name="Male" fill="#f54394" />
-  //                 <Cell name="Female" fill="#5a8dee" />
-  //                 <Cell name="Others" fill="#2cc6c6" />
-  //               </Pie>
-  //               <Legend
-  //                 iconType="square"
-  //                 layout="horizontal"
-  //                 HorizontalAlign="end"
-  //                 align="center"
-  //                 wrapperStyle={{'margin-bottom': 25, padding: 25}}
-  //               />
-  //             </PieChart>
-  //           </ResponsiveContainer>
-  //         </div>
-  //       </div>
-  //     )
-  //   }
-
   renderSuccessView = () => {
     const {vaccineData, isLoading} = this.state
     return isLoading ? (
--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -1,45 +1,38 @@
 import './index.css'
-import {
-  ResponsiveContainer,
-  Bar,
-  BarChart,
-  XAxis,
-  YAxis,
-  Legend,
-} from 'recharts'
+import {ResponsiveContainer, PieChart, Pie, Cell, Legend} from 'recharts'
 
 const VaccinationByGender = props => {
   const {vaccineData} = props
-
-  const dataFormatter = n => {
-    if (n > 1000) {
-      return `${(n / 100).toString()}k`
-    }
-    return n.toString()
-  }
 
   return (
     <div className="mini-container">
       <h1 className="chart-title">CoWIN Vaccination in India</h1>
       <div className="chart-container">
-        <h1 className="chart-title">Vaccination Coverage</h1>
-        <ResponsiveContainer width="100%" height={500}>
-          <BarChart
-            data={vaccineData.last_7_days_vaccination}
-            margin={{top: 5}}
-          >
-            <XAxis
-              dataKey="vaccine_date"
-              tick={{stroke: '#6c757d', strokewidth: 2}}
+        <h1 className="chart-title">Vaccination by Gender</h1>
+        <ResponsiveContainer width="100%" height={300}>
+          <PieChart>
+            <Pie
+              cx="50%"
+              cy="60%"
+              data={vaccineData.vaccination_by_gender}
+              startAngle={180}
+              endAngle={0}
+              innerRadius="40%"
+              outerRadius="80%"
+              dataKey="count"
+            >
+              <Cell name="Male" fill="#f54394" />
+              <Cell name="Female" fill="#5a8dee" />
+              <Cell name="Others" fill="#2cc6c6" />
+            </Pie>
+            <Legend
+              iconType="square"
+              layout="horizontal"
+              HorizontalAlign="end"
+              align="center"
+              wrapperStyle={{'margin-bottom': 25, padding: 25}}
             />
-            <YAxis
-              tickFormatter={dataFormatter}
-              tick={{stroke: '#6c757d', strokewidth: 0}}
-            />
-            <Legend wrapperStyle={{padding: 25}} />
-            <Bar dataKey="dose_1" name="Dose 1" barSize="20%" fill="#5a8dee" />
-            <Bar dataKey="dose_2" name="Dose 2" barSize="20%" fill="#f54394" />
-          </BarChart>
+          </PieChart>
         </ResponsiveContainer>
       </div>
     </div>
